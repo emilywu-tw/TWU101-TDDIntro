8ceef9f07288bf7d419ac248ac43c545d6543ce8
emilywu-tw <emilywuthoughtworks@gmail.com>
2017-12-23 00:53:30 -0500

    completed Account exercise

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,29 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        // This function deposits the given amount into the account.
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        // This function attempts to decrement the balance by amount. If that results in a negative value,
+        // the withdrawal will be denied.
+        if (balance - amount < 0) {
+            System.out.printf("You only have $%d in your account. Cannot withdraw $%d.%n", balance, amount);
+        }
+        else {
+            balance -= amount;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
