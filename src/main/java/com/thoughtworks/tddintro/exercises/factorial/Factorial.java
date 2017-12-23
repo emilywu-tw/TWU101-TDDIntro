a3d2defeb523bfab35f06b6a1d558ba27190a48f
emilywu-tw <emilywuthoughtworks@gmail.com>
2017-12-23 00:39:39 -0500

    completed Factorial exercise

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,16 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        // This method computes the factorial of the given integer i.
+        if (i < 0) {
+            throw new IllegalArgumentException("Cannot compute factorial of a negative number");
+        }
+
+        int factorial = 1;
+
+        for (int n = 1; n < i + 1; n++) {
+            factorial *= n;
+        }
+        return factorial;
     }
 }
